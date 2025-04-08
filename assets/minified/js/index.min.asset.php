51c547c242da29a3dfc1391426e029def5c1a2e7
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2025-04-08 08:40:45 +0300

    tabs 1.0.1

--- assets/minified/js/index.min.asset.php
+++ assets/minified/js/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-polyfill'), 'version' => '968f6fa1a87f38798e59');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-polyfill'), 'version' => 'b7a4fb0cc8d6c32e593d');
